3f867fc38e7ca09b0f4ba769df7e270e657fbeee
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-01-22 17:29:38 +0100

    GraphQL id type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,4 @@
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = require("graphql");
 const _ = require("lodash");
 
 //dummy data
@@ -11,7 +11,7 @@ const books = [
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
@@ -22,7 +22,7 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         //Code to get data from db/other source
         return _.find(books, { id: args.id });
