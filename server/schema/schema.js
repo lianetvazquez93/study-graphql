a6cb86921fdc326aec0082aa9bed8e05206c653f
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-01-22 16:44:23 +0100

    Root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,4 @@
-const { GraphQLOjectType, GraphQLString } = require("graphql");
+const { GraphQLOjectType, GraphQLString, GraphQLSchema } = require("graphql");
 
 const BookType = new GraphQLOjectType({
   name: "Book",
@@ -7,4 +7,21 @@ const BookType = new GraphQLOjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLOjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        //Code to get data from db
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
