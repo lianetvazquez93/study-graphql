0cbc3cca9c4f1b1b0590a5497d2ad6449683d160
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-01-22 23:32:20 +0100

    Mutation addAuthor

--- server/models/book.js
+++ server/models/book.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const bookSchema = mongoose.Schema({
   name: String,
   genre: String,
-  authorId: mongoose.SchemaTypes.ObjectId,
+  authorId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Author",
+  },
 });
 
 module.exports = mongoose.model("Book", bookSchema);
