cf6b58df527bf5828a522951eebfaeca6c4e9885
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-01-23 10:18:49 +0100

    GraphQL NonNull

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,6 +5,7 @@ const {
   GraphQLID,
   GraphQLInt,
   GraphQLList,
+  GraphQLNonNull,
 } = require("graphql");
 const _ = require("lodash");
 
@@ -101,8 +102,8 @@ const Mutation = new GraphQLObjectType({
     addAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve(parent, args) {
         let author = new Author({ name: args.name, age: args.age });
@@ -112,9 +113,9 @@ const Mutation = new GraphQLObjectType({
     addBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        authorId: { type: GraphQLID },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) },
       },
       resolve(parent, args) {
         let book = new Book({ name: args.name, genre: args.genre, authorId: args.authorId });
