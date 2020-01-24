888be32ffd237757c39080efb64af2a5d7f9c4ee
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-01-24 17:32:23 +0100

    Rendering data in a component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,12 +12,20 @@ const getBooksQuery = gql`
 `;
 
 class BookList extends React.Component {
+  displayBooks() {
+    const { data } = this.props;
+    if (data.loading) {
+      return <div>Loading books...</div>;
+    }
+
+    return data.books.map(book => {
+      return <li key={book.id}>{book.name}</li>;
+    });
+  }
   render() {
     return (
       <div>
-        <ul className="book-list">
-          <li>Book Name</li>
-        </ul>
+        <ul className="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
